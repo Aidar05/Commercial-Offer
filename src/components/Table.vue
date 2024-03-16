1ed1f55271a6390c287479227333b4203863835c
Aidar <sharipovaydar200515@gmail.com>
2024-03-16 15:20:20 +0600

    Основную верстку всю сделал. Теперь красивый css нужен

---
+++ src/components/Table.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="table">
+    <div class="table-header">
+      <h4>{{ tableHeader }}</h4>
+    </div>
+    
+    <div class="table-content">
+      <div class="table-element" v-for="tableElement in tableContent">
+        <div class="column-name">
+          <span>{{ tableElement.columnName }}</span>
+        </div>
+        
+        <ul class="column-content">
+          <li v-for="(elements, index) in tableElement.points" :key="index">
+            {{ elements }}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default{
+  name: "TableElement",
+  props: {
+    tableHeader: String,
+    tableContent: Array
+  }
+}
+</script>
